f346cd12ba5510a06e794f0c2effe02f41bd5822
Ryan Nowakowski <tubaman@fattuba.com>
2013-01-26 11:45:23 -0600

    fix issues

--- newproj/settings/common.py
+++ newproj/settings/common.py
@@ -91,7 +91,6 @@ TEMPLATE_LOADERS = (
     'django.template.loaders.filesystem.Loader',
     'django.template.loaders.app_directories.Loader',
 #     'django.template.loaders.eggs.Loader',
-    os.path.join(FILE_ROOT, 'templates'),
 )
 
 MIDDLEWARE_CLASSES = (
@@ -113,6 +112,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    os.path.join(FILE_ROOT, 'templates'),
 )
 
 INSTALLED_APPS = (
