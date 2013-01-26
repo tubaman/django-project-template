ad05f6dd54b06e6e0740c8ace741fab11861f9a9
Ryan Nowakowski <tubaman@fattuba.com>
2013-01-26 11:48:13 -0600

    FILE_ROOT is the top-level

--- newproj/settings/common.py
+++ newproj/settings/common.py
@@ -2,7 +2,7 @@
 import os.path
 import posixpath
 
-FILE_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+FILE_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), '../..'))
 WEB_ROOT = '/'
 
 DEBUG = True
